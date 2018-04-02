0943d97f3f0ff2658909cd7b654963557d739a4c
Matias Keveri <keveri@users.noreply.github.com>
2018-04-02 19:27:08 +0300

    Clarify test names

--- tests/api_functionality.rs
+++ tests/api_functionality.rs
@@ -16,38 +16,38 @@ fn test_client() -> rocket::local::Client {
     Client::new(rocket).expect("valid rocket instance")
 }
 
+
 #[test]
-fn it_works() {
+fn api_index_reponds_with_ok() {
     let client   = test_client();
     let response = client.get("/").dispatch();
     assert_eq!(response.status(), Status::Ok);
 }
 
 #[test]
-fn shortening_api() {
+fn shortening_api_returns_expected_id_for_the_first_url() {
     let client   = test_client();
+    let first_id = "gY";
     let request  = client.post("/")
         .header(ContentType::Form)
         .body(r#"url=https://www.rust-lang.org"#);
     let mut response = request.dispatch();
     assert_eq!(response.status(), Status::Ok);
-    assert_eq!(response.body_string(), Some("gY".into()));
+    assert_eq!(response.body_string(), Some(first_id.into()));
 }
 
 #[test]
-fn find_api_error() {
+fn find_api_returns_an_error_when_requested_with_an_invalid_id() {
     let client   = test_client();
-    let response = client.get("/non-existing-ID").dispatch();
+    let response = client.get("/invalid-ID").dispatch();
     assert_eq!(response.status(), Status::NotFound);
 }
 
-
 #[test]
-fn shortening_and_finding_api_flow() {
+fn shortening_api_creates_an_working_id_and_redirects_from_find_api_with_it() {
     let client   = test_client();
     let first_id = "gY";
-
-    let request = client.post("/")
+    let request  = client.post("/")
         .header(ContentType::Form)
         .body(r#"url=https://www.rust-lang.org"#);
     let mut response = request.dispatch();
